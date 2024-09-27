c18f2be040dbef7344ef0b9f7cd55323d27f300f
Yash2428 <yr673317@gmail.com>
2024-09-27 13:56:02 +0530

    Create README.m

---
+++ README.m
@@ -0,0 +1,12 @@
+- 👋 Hi, I’m @Yash2428
+- 👀 I’m interested in software development 
+- 🌱 I’m currently learning computer languages from scratch 
+- 💞️ I’m looking to collaborate on future projects 
+- 📫 How to reach me linkedin 
+- 😄 Pronouns: ...
+- ⚡ Fun fact: ...
+
+<!---
+Yash2428/Yash2428 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
